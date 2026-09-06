b59cf95601b5202960c06677dd8b23d04c517e54
JEWEL HOSSAIN <jewelhossain9091@gmail.com>
2026-09-06 12:42:03 +0530

    Use env variable for API base URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,20 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api",
 });
 
-// Endpoints that should NEVER get an Authorization header attached,
-// since they're meant to be called by logged-out users.
-const PUBLIC_ENDPOINTS = ["/register/", "/token/", "/token/refresh/"];
-
 api.interceptors.request.use((config) => {
-  const isPublic = PUBLIC_ENDPOINTS.some((path) => config.url.includes(path));
-  if (!isPublic) {
-    const token = localStorage.getItem("access_token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
+  const token = localStorage.getItem("access_token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
