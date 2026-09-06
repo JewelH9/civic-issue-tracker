f0234cc29b06f4e5e94e4211f13377aaf6430f45
JEWEL HOSSAIN <jewelhossain9091@gmail.com>
2026-09-06 13:25:39 +0530

    Move photo storage to Cloudinary for persistent production uploads

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -35,7 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
 
     'rest_framework',
     'corsheaders',
@@ -45,6 +47,13 @@ INSTALLED_APPS = [
     'issues',
 ]
 
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
